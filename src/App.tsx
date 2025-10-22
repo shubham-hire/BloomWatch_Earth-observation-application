36d0b7c4d9184ec2a5903913ec11485e549b0257
Shubham Hire <shubham.hire2006@gmail.com>
2025-10-22 22:07:17 +0530

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import L from "leaflet";
 import "leaflet/dist/leaflet.css";
 import "./App.css";
 
-// Fix for default markers in react-leaflet
+// Fix for default markers in react-leaflet . 
 delete (L.Icon.Default.prototype as any)._getIconUrl;
 L.Icon.Default.mergeOptions({
   iconRetinaUrl:
